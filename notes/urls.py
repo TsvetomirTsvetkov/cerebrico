265f52f5d632d2e22cdbab1a5b6bbd4d7e283d90
TsvetomirTsvetkov <tsvetomir.e.tsvetkov@gmail.com>
2024-03-23 01:29:57 +0200

    Add views, link views, create base template

--- notes/urls.py
+++ notes/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("create/", views.create, name="create"),
+    path("folders/", views.folders, name="folders"),
+    path("tasks/", views.tasks, name="tasks"),
+    path("board/", views.board, name="board"),
+    path("settings/", views.settings, name="settings"),
 ]
--- notes/views.py
+++ notes/views.py
@@ -7,3 +7,33 @@ def index(request):
         request,
         'notes/index.html',
     )
+
+def create(request):
+    return render(
+        request,
+        'notes/create.html',
+    )
+
+def folders(request):
+    return render(
+        request,
+        'notes/folders.html',
+    )
+
+def tasks(request):
+    return render(
+        request,
+        'notes/tasks.html',
+    )
+
+def board(request):
+    return render(
+        request,
+        'notes/board.html',
+    )
+
+def settings(request):
+    return render(
+        request,
+        'notes/settings.html',
+    )
