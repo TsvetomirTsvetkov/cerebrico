609c739fa427903b1a848da7e75a91891d9f28c0
TsvetomirTsvetkov <tsvetomir.e.tsvetkov@gmail.com>
2024-05-31 23:12:19 +0300

    Start work on tasks functionality and persistent checkboxes

--- notes/views/tasks.py
+++ notes/views/tasks.py
@@ -3,31 +3,47 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
-# TODO: Possibly remove from Preprocessor and make common
+# TODO: Possibly remove from Preprocessor and make common?
 from notes.src.preprocessors import KeywordPreprocessor
+
+def update_file(file, line):
+    old_line = line
+
+    try:
+        if line[:6] != "[DONE]":
+            line = "[DONE]" + line
+        else:
+            line = line[6:]
+    except:
+        # TODO: Possibly handle this
+        pass
+
+    with open(file.upload.path, 'rw') as f:
+        file_contents = f.read()
+        file_contents.replace(old_line, line)
+        f.write(file_contents)
 
 @login_required
 def tasks(request):
     files = User.objects.get(username=request.user).file_set.all()
     user_settings = User.objects.get(username=request.user).usersettings_set.all()
 
-    all_tasks = []
-    
+    all_tasks_dict = {}
+
     for file in files:
+        all_tasks_dict[file.title] = []
         with file.upload.open('r') as f:
             lines = f.readlines()
             for line in lines:
-                print(line)
                 m = KeywordPreprocessor.check_for_patterns(line, user_settings)
-
                 if m:
-                    all_tasks.append(line)
+                    all_tasks_dict[file.title].append(line)
 
     return render(
         request,
         'notes/tasks.html',
         {
             "files": files, 
-            "all_tasks": all_tasks
+            "all_tasks_dict": all_tasks_dict
         }
     )
